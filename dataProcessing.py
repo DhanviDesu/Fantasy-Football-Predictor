9ddce3086eba1691aa000c679f5e073cdc94e99d
Dhanvi Desu <spis19ak@its-cseb240-16.ucsd.edu>
2019-09-04 15:04:56 -0700

    Final Project MVP

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import re
+import operator
 
 rawData = open("dataPretty.html", "r")
 qB = open("qBNames.html", "r")
@@ -10,13 +11,78 @@ data = stats.find_all("div", {'class' : "kyber-table-body-cell kyber-table-body-
 
 names = people.find_all("a", {'class' : ""})
 
-#for message in data:
-#    try:
-#        wantedData = int(message.text.strip())
-#    except:
-#        continue
-#    print(wantedData)
+statsList = []
+namesList = []
+fullDict = {}
+finalDict = {}
+finalRank = []
+weights = [40, 30, -10, 25, 15]
+
+#formula:  rank each person based on each cat, then rank by highest ave in each cat, (lowest in inteceptions), 
+#35*pYards+25 * ptd + 10*int + 20*ryards + 10*rtd
+
+
+
+
+def getPosition(statNum, toCalc):
+    sortedPos = sorted(toCalc.items(), key=lambda e: e[1][statNum])
+    i = 0
+    while i < len(sortedPos):
+        sortedPos[i][1][statNum] = len(sortedPos) -i
+        i += 1
+    
+    return sortedPos
+
+def rankings(toCalc):
+    tempRank = []
+    statType = 0
+    while statType < 5:
+        tempRank = getPosition(statType, toCalc)
+        for player in tempRank:
+            if player[0] not in finalDict:
+                finalDict[player[0]] =  player[1][statType] * weights[statType]
+            else:
+                finalDict[player[0]] = finalDict[player[0]] + player[1][statType] * weights[statType]  
+        statType += 1
+    
+    sortedFinal = sorted(finalDict.items(), key = lambda e: e[1])
+    print(sortedFinal)
+
+for message in data:
+    try:
+        wantedData = int(message.text.strip())
+    except:
+        continue
+    #print(wantedData)
+    statsList.append(wantedData)
 
 for eachName in names:
     wantedName = eachName.text.strip()
-    print(wantedName)
+    #print(wantedName)
+    namesList.append(wantedName)
+
+# 3(pyards) 4(ptd) 5(int) 7(ryards) 8(rtd)
+counter = 0
+for name in namesList:
+    fullDict[name] = [ statsList[10*counter + 3] ]
+    index = 0
+    while index < 10:
+        if index == 4 or index == 5 or index == 7 or index == 8:
+            fullDict[name].append( statsList[10*counter + index] )
+        index += 1
+    counter += 1
+
+
+
+
+
+
+
+rankings(fullDict)
+
+#print(fullDict)
+#getPosition (2, fullDict)
+
+
+#print(statsList)
+#print(namesList)
